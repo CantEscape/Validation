52a410093c980836ff5330e2aa05d15027e667d4
cantescape <james.kirkwood322@googlemail.com>
2015-02-17 15:54:53 +0100

    name changing

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 include "lib/Validator.php";
 
-use CantEscape\Validator as v;
+use CantEscape\Validator;
 
 //Create new Validator
-$v = new v();
+$v = new Validator();
 
 var_dump(array(
     $v->email("someemail@mail.com")->email("one@two@.net", "james@me.com")->debrief()
