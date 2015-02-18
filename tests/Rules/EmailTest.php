81bdda2dc4213effec473a37edc4f97e0186e40e
cantescape <james.kirkwood322@googlemail.com>
2015-02-18 21:04:11 +0100

    fix

--- tests/Rules/EmailTest.php
+++ tests/Rules/EmailTest.php
@@ -3,6 +3,8 @@ use CantEscape\Validator;
 
 class EmailTest extends \PHPUnit_Framework_TestCase
 {
+    include "../../lib/Validator.php";
+    
     public function testEmail()
     {
         $v = new Validator();
